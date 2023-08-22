49c1e0095d2e6e3598d1ce3e75a4d1349cb7118a
unknown <abdallahaliahmed517@gmail.com>
2023-08-22 15:09:48 +0300

    AAA

--- CA02TaskReturnsValue/Program.cs
+++ CA02TaskReturnsValue/Program.cs
@@ -1,12 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System;
 
 //Task<DateTime> task = Task.Run(() => DateTime.Now);
 Task<DateTime> task = Task.Run(GetCurrentDateTime);
 //Console.WriteLine(task);           // -> print "System.Threading.Tasks.Task`1[System.DateTime]"
-Console.WriteLine(task.Result);    // -> print "8/22/2023 2:54:00 PM"
+//Console.WriteLine(task.Result);    // -> print "8/22/2023 2:54:00 PM"
 
-
+Console.WriteLine(task.GetAwaiter().GetResult());
 static DateTime GetCurrentDateTime() => DateTime.Now;
 
 
