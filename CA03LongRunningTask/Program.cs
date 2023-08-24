5eaf8b81f8337b8efcfbf96434b2bd360cf5fa0d
unknown <abdallahaliahmed517@gmail.com>
2023-08-24 11:30:52 +0300

    AAA

---
+++ CA03LongRunningTask/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+var task = Task.Factory.StartNew(() => RunLongTask(),
+TaskCreationOptions.LongRunning);
+Console.WriteLine(task);
+
+
+static void RunLongTask()
+{
+    Thread.Sleep(3000);
+    ShowThreadInfo(Thread.CurrentThread);
+    Console.WriteLine("Completed");
+}
+static void ShowThreadInfo(Thread th)
+{
+    Console.WriteLine($"TID: {th.ManagedThreadId}, Pooled: {th.IsThreadPoolThread}, Background: {th.IsBackground}");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
